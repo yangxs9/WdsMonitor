5b393b48ce9ac09cca0a5df41f0f5d672f5f5340
Xinsong Yang <yangxinsong93@gmail.com>
2017-06-03 21:47:54 +0800

    wdsMonitor

---
+++ lsnWdsRobot.py
@@ -0,0 +1,7 @@
+import wdsMonitor
+
+url = 'https://wds.modian.com/ranking_list?pro_id=4135'
+group = '空'
+interval = 60
+
+wdsMonitor(url, group, interval)
---
+++ wdsMonitor.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+from urllib import request
+from bs4 import BeautifulSoup
+import time
+import os
+
+url = 'https://wds.modian.com/ranking_list?pro_id=4135'
+group = '空'
+
+def getHtml(url):
+    req = request.Request(url)
+    req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+    with request.urlopen(req) as f:
+        if f.status == 200:
+            return f.read().decode('utf-8')
+        else:
+            print('cant get the page')
+            return None
+
+def getWdsData(html):
+    userMoney = {}
+    soup = BeautifulSoup(html, 'lxml')
+    userTags = soup.select('#wrapper_jj .nickname')
+    moneyTags = soup.select('#wrapper_jj .money')
+    for i in range(len(userTags)):
+        name = userTags[i].string
+        moneyStr = moneyTags[i].string[1:]
+        money = float(moneyStr)
+        userMoney[name] = money
+    return userMoney
+
+def getUserMoney(url):
+    html = getHtml(url)
+    if html != None:
+        userMoney = getWdsData(html)
+        return userMoney
+    else:
+        return None
+
+def getAddedUserMoney(userMoney, newUserMoney):
+    addedUserMoney = {}
+    for user in newUserMoney:
+        if userMoney[user] == None:
+            addedUserMoney[user] = newUserMoney[user]
+        elif userMoney[user] < newUserMoney[user]:
+            addedUserMoney[user] = newUserMoney[user] - userMoney[user]
+    return addedUserMoney
+
+def qqReport(addedUserMoney, group):
+    for user in addedUserMoney: 
+        msg = user + ' 刚刚集资了' + str(addedUserMoney[user]) + '元'
+        print(msg)
+        cmd = 'qq send group ' + group + ' ' + msg
+        os.system(cmd)
+
+def wdsMonitor(url, group, interval):
+    userMoney = getUserMoney(url)
+    while True:
+        time.sleep(interval)
+        newUserMoney = getUserMoney(url)
+        addedUserMoney = getAddedUserMoney(userMoney, newUserMoney)
+        if len(addedUserMoney) == 0:
+            print('no change')
+        else:
+            qqReport(addedUserMoney, group)
+            userMoney = newUserMoney
+
+wdsMonitor(url, group, 10)
